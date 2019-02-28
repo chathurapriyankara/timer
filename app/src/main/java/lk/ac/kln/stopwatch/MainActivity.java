53ed5c90ca7fdaeb96b2462c2396ab165db902b5
Chathura <priyankarahac@gmail.com>
2019-02-28 23:47:48 +0530

    Added all project files

---
+++ app/src/main/java/lk/ac/kln/stopwatch/MainActivity.java
@@ -0,0 +1,72 @@
+package lk.ac.kln.stopwatch;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int countDown = 99;
+    private boolean running = false;
+    private boolean wasRunning = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        countDown();
+    }
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        wasRunning = running;
+//        running = false;
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if(wasRunning) {
+//            running = true;
+//        }
+//    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(wasRunning) {
+            running = true;
+        }
+    }
+
+    protected void clickStart(View view) {
+        running = true;
+    }
+
+    private void countDown() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                final TextView textView = findViewById(R.id.textView3);
+                textView.setText(String.format("%2d",countDown));
+                if(countDown==0) {
+                    countDown = 100;
+                }
+                if(running) {
+                    countDown--;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+}
